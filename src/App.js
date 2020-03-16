b68aa4d267c30ad2a4a1bf40adb5525f5e9bde71
evb0110 <7149553@gmail.com>
2020-03-16 23:27:59 +0300

    adding component stubs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Dashboard />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <Dashboard/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
-                <h1>
-                    Dashboard
-                </h1>
+                <h1>Dashboard</h1>
+                <ProjectItem/>
+                <ProjectItem/>
+                <ProjectItem/>
+                <ProjectItem/>
             </div>
         );
     }
