9876b2d892f3c4283d3155072006531a59ce6780
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2020-03-16 23:32:50 +0300

    reformat

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Header/>
-                <Dashboard/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <Dashboard/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,17 +2,17 @@ import React, {Component} from 'react';
 import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Dashboard</h1>
-                <ProjectItem/>
-                <ProjectItem/>
-                <ProjectItem/>
-                <ProjectItem/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        <ProjectItem/>
+        <ProjectItem/>
+        <ProjectItem/>
+        <ProjectItem/>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
