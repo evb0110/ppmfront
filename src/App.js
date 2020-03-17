8ac5e804419b395354f132c9a2fe1623e2cf2a89
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2020-03-17 17:04:30 +0300

    bootstrap, fa etc

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, {Component} from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import AddProject from './components/Project/AddProject';
+import Route from 'react-router-dom/es/Route';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header/>
-        <Dashboard/>
-      </div>
+      <Router>
+        <div className="App">
+          <Header/>
+          <Route exact path="/dashboard" component={Dashboard}/>
+          <Route exact path="/addProject" component={AddProject}/>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react';
 import ProjectItem from './Project/ProjectItem';
+import CreateProjectButton from './Project/CreateProjectButton';
 
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1>Dashboard</h1>
-        <ProjectItem/>
-        <ProjectItem/>
-        <ProjectItem/>
-        <ProjectItem/>
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br/>
+              <CreateProjectButton/>
+              <br/>
+              <hr/>
+
+              <ProjectItem/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
